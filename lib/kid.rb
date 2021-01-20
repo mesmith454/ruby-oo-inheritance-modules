048a635bb6a82e2df1d6180dad966027738b25f5
mesmith454 <mesmith454@gmail.com>
2021-01-20 14:59:07 -0800

    done

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @dance_moves = []
+    end
+
+    def see_ballet
+        #sees a performance and learns a move to practice
+    end
+
+    def practice_dance
+    end
+
+end 
